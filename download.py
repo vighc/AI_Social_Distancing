a680fb4ea35b558cc5cce50de6b69154f1a7624a
Vighneshwara C <61262139+vighc@users.noreply.github.com>
2020-07-14 12:29:08 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,7 +1,15 @@
-import urllib
+import urllib.request
+
+print('Beginning file download with urllib2...')
+
+url = 'https://pjreddie.com/media/files/yolov3.weights'
+urllib.request.urlretrieve(url, './models/yolov3.weights')
+
+'''import urllib
 linkToFile = "https://pjreddie.com/media/files/yolov3.weights"
 localDestination = "./models/yolov3.weights"
 resultFilePath, responseHeaders = urllib.urlretrieve(linkToFile, localDestination)
 
 #%cd ./models/
 #!wget https://pjreddie.com/media/files/yolov3.weights
+'''
